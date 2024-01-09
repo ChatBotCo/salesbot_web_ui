647e2517af54d322966daefe32aab432d8ed387b
Shawn Cook <sfncook@gmail.com>
2024-01-09 07:33:24 -0800

    Format avatar response

--- src/components/chat/AvatarResponse.jsx
+++ src/components/chat/AvatarResponse.jsx
@@ -8,10 +8,8 @@ export const AvatarResponse = () => {
 
   if(company && lastAvatarResponseText) {
     return (
-      <div className='grow p-4 md:pl-0 md:pr-0 italic'>
-        From
-        <span className='font-semibold ml-1'>{company.name}</span>:<br/>
-        <span className='text-blue-500'>"{lastAvatarResponseText}"</span>
+      <div className='w-full max-w-lg max-h-56 p-4 md:pl-0 md:pr-0 italic text-blue-500 overflow-y-scroll'>
+        {lastAvatarResponseText}
       </div>
     );
   } else return <span></span>

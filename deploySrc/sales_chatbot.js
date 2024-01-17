67e8f6d69d70c13d4175d24ad793bf8a8a2e2781
Shawn Cook <sfncook@gmail.com>
2024-01-17 12:50:34 -0800

    v0.8

--- deploySrc/sales_chatbot.js
+++ deploySrc/sales_chatbot.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded",
     // Load the chatbot's CSS
     var chatbotCSS = document.createElement('link');
     // chatbotCSS.href = 'http://localhost:5173/deleteme/index.css';
-    chatbotCSS.href = 'https://kelichatbot2.blob.core.windows.net/salesbot-assets/index-v0.7.css';
+    chatbotCSS.href = 'https://kelichatbot2.blob.core.windows.net/salesbot-assets/index-v0.8.css';
     chatbotCSS.rel = 'stylesheet';
     chatbotCSS.type = 'text/css';
     document.head.appendChild(chatbotCSS);
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded",
     // Load the chatbot's JavaScript bundle
     var chatbotScript = document.createElement('script');
     // chatbotScript.src = 'http://localhost:5173/deleteme/index.js';
-    chatbotScript.src = 'https://kelichatbot2.blob.core.windows.net/salesbot-assets/index-v0.7.js';
+    chatbotScript.src = 'https://kelichatbot2.blob.core.windows.net/salesbot-assets/index-v0.8.js';
     chatbotScript.async = true;
     chatbotScript.onload = function() {
       // Initialize the chatbot here if needed

9301940b48f811b7c8fca36b656abc4acb70eb20
Shawn Cook <sfncook@gmail.com>
2024-03-08 14:48:52 -0800

    v0.17

--- deploySrc/sales_chatbot.js
+++ deploySrc/sales_chatbot.js
@@ -10,14 +10,14 @@ document.addEventListener("DOMContentLoaded",
 
     // Load the chatbot's CSS
     var chatbotCSS = document.createElement('link');
-    chatbotCSS.href = 'https://kelichatbot2.blob.core.windows.net/salesbot-assets/index-v0.16.css';
+    chatbotCSS.href = 'https://kelichatbot2.blob.core.windows.net/salesbot-assets/index-v0.17.css';
     chatbotCSS.rel = 'stylesheet';
     chatbotCSS.type = 'text/css';
     document.head.appendChild(chatbotCSS);
 
     // Load the chatbot's JavaScript bundle
     var chatbotScript = document.createElement('script');
-    chatbotScript.src = 'https://kelichatbot2.blob.core.windows.net/salesbot-assets/index-v0.16.js';
+    chatbotScript.src = 'https://kelichatbot2.blob.core.windows.net/salesbot-assets/index-v0.17.js';
     chatbotScript.async = true;
     document.body.appendChild(chatbotScript);
   }
